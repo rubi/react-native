712daabf242ad1b0cfec2d4a645c6321d85f2566
seven <juanleyiji@gmail.com>
2017-11-03 11:04:43 +0800

    change tabbar and navbar style

--- Axis/App.js
+++ Axis/App.js
@@ -10,17 +10,56 @@ import {
 registerScreens();
 registerScreenVisibilityListener();
 
-const tabs = [{
-	label: 'Class',
-	screen: 'example.T1',
-	icon: require('./app/img/list.png'),
-	title: 'Schedule Class',
-}, {
-	label: 'Notifications',
-	screen: 'example.T2',
-	icon: require('./app/img/swap.png'),
-	title: 'Schedule Availability',
-}];
+const tabs = [
+	{
+		label: 'Class',
+		screen: 'example.T1',
+		icon: require('./app/img/list.png'),
+		title: 'Schedule Class',
+		iconInsets: { // add this to change icon position (optional, iOS only).
+			top: 6, // optional, default is 0.
+			left: 0, // optional, default is 0.
+			bottom: -6, // optional, default is 0.
+			right: 0 // optional, default is 0.
+		},
+		navigatorStyle: {
+			navBarTextFontSize: 15
+		},
+		navigatorButtons: {}
+	},
+	{
+		label: 'Availability',
+		screen: 'example.T2',
+		icon: require('./app/img/swap.png'),
+		title: 'Schedule Availability',
+		iconInsets: { // add this to change icon position (optional, iOS only).
+			top: 6, // optional, default is 0.
+			left: 0, // optional, default is 0.
+			bottom: -6, // optional, default is 0.
+			right: 0 // optional, default is 0.
+		},
+		navigatorStyle: {
+			navBarTextFontSize: 15
+		},
+		navigatorButtons: {}
+	},
+	{
+		label: 'Notifications',
+		screen: 'example.T2',
+		icon: require('./app/img/transform.png'),
+		title: 'Schedule Notifications',
+		iconInsets: { // add this to change icon position (optional, iOS only).
+			top: 6, // optional, default is 0.
+			left: 0, // optional, default is 0.
+			bottom: -6, // optional, default is 0.
+			right: 0 // optional, default is 0.
+		},
+		navigatorStyle: {
+			navBarTextFontSize: 15
+		},
+		navigatorButtons: {}
+	}
+];
 
 /*if (Platform.OS === 'android') {
 	tabs.push({
@@ -36,19 +75,19 @@ Navigation.startTabBasedApp({
 	tabs,
 	animationType: Platform.OS === 'ios' ? 'slide-down' : 'fade',
 	tabsStyle: {
-		tabBarBackgroundColor: '#003a66',
-		tabBarButtonColor: '#ffffff',
+		tabBarBackgroundColor: '#F8F9F9',
+		tabBarButtonColor: '#2E4053',
 		tabBarSelectedButtonColor: '#ff505c',
 		tabFontFamily: 'BioRhyme-Bold',
 	},
 	appStyle: {
-		tabBarBackgroundColor: '#003a66',
+		tabBarBackgroundColor: '#F8F9F9',
 		navBarButtonColor: '#ffffff',
-		tabBarButtonColor: '#ffffff',
+		tabBarButtonColor: '#2E4053',
 		navBarTextColor: '#ffffff',
 		tabBarSelectedButtonColor: '#ff505c',
-		navigationBarColor: '#003a66',
-		navBarBackgroundColor: '#003a66',
+		navigationBarColor: '#1F618D',
+		navBarBackgroundColor: '#1F618D',
 		statusBarColor: '#002b4c',
 		tabFontFamily: 'BioRhyme-Bold',
 	}
