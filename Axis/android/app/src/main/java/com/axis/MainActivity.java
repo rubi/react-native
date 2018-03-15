78db7d10dbe7093ed92669981303771c1a03cffd
seven <juanleyiji@gmail.com>
2018-03-15 15:40:28 +0800

    add generate key

--- Axis/android/app/src/main/java/com/axis/MainActivity.java
+++ Axis/android/app/src/main/java/com/axis/MainActivity.java
@@ -6,11 +6,11 @@ import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends SplashActivity {
 
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this);
         super.onCreate(savedInstanceState);
-    }
+    }*/
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
